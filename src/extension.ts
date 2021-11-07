71f9d857b3d35acb9c1f91125e4167893bfa748a
cpf23333 <3288765969@qq.com>
2021-11-07 23:36:06 +0800

    注册了一个右键菜单：设置备注，目前只获取了输入的备注内容和右键的文件夹路径，未完成

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,42 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "file-note" is now active!');
+  vscode.window.showInformationMessage("进来", "确定").then((res) => {
+    // console.log(res);
+    vscode.window.showInformationMessage("点了确定");
+  });
+  let setNote = vscode.commands.registerCommand("file-note.setNote", (rest) => {
+    vscode.window.showInformationMessage("开始设置备注");
+    // vscode.window.showInputBox({ value: JSON.stringify([rest]) });
+    vscode.window
+      .showInputBox({ title: "请输入本文件夹的备注" })
+      .then((res) => {
+        vscode.window.showInformationMessage(res || "");
+      });
+  });
+  context.subscriptions.push(setNote);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('file-note.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from file Note!');
-	});
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "file-note" is now active!');
 
-	context.subscriptions.push(disposable);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "file-note.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from file Note!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated

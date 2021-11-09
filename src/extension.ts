cba23694f72abcd0fd2bfcdda70b7516bc0cb8ac
cpf23333 <3288765969@qq.com>
2021-11-09 21:09:49 +0800

    功能完成，等会看看怎么上架

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,6 @@ class CountDecorationProvider {
   }
   provideFileDecoration(uri: vscode.Uri): vscode.FileDecoration {
     let relativePath = getRelativePath(uri);
-    // console.log(relativePath);
     let txt = getNote(relativePath);
     if (!txt) {
       return {};
@@ -28,38 +27,21 @@ class CountDecorationProvider {
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   init();
+  let provider = new CountDecorationProvider();
   let setNote = vscode.commands.registerCommand(
     "file-notes.setNote",
     (uri: vscode.Uri) => {
+      let relativePath = getRelativePath(uri);
+      let val = getNote(relativePath);
+      provider.provideFileDecoration(uri);
       vscode.window
-        .showInputBox({ title: "请输入本文件夹的备注" })
+        .showInputBox({ title: "请输入本文件或文件夹的备注", value: val })
         .then((txt = "") => {
-          let relativePath = getRelativePath(uri);
           saveNote(relativePath, txt);
-          let provider = new CountDecorationProvider();
-          provider.provideFileDecoration(uri);
         });
     }
   );
   context.subscriptions.push(setNote);
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "file-notes" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "file-notes.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from file Note!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,9 +7,9 @@ export function getBasePath(uri: vscode.Uri) {
 }
 export function getRelativePath(pathUri: vscode.Uri): string {
   let basePath = getBasePath(pathUri)?.uri.fsPath || "";
-  basePath = basePath.slice(0, basePath.length - 1);
+  basePath = basePath.slice(0, basePath.length);
   let path = pathUri.fsPath;
-  return path.replace(basePath, "").replace(/\\/g, "");
+  return path.replace(basePath, "").replace(/\\/g, "/");
 }
 
 export let notes: any = {};
@@ -29,11 +29,10 @@ export function getNote(key: string) {
 }
 /**初始化 */
 export function init() {
-  console.log("初始化");
   // 如果插件所属的工作区不存在储存路径，则创建一个
   let settingJsonPath = (vscode.workspace.workspaceFolders || [])[0]?.uri
     .fsPath;
-  let url = path.join(settingJsonPath, ".vscode");
+  let url = path.join(settingJsonPath, ".vscode").replace(/\\/g, "/");
   if (!fs.existsSync(url)) {
     fs.mkdirSync(url);
   }
@@ -42,16 +41,20 @@ export function init() {
     fs.writeFileSync(path.join(url, "file-notes.json"), JSON.stringify({}));
     notes = {};
   } else {
-    let content = fs.readFileSync(path.join(url, "file-notes.json"), {
-      encoding: "utf-8",
-    });
-    notes = content || {};
-    console.log("还原数据", notes);
+    let jsonPath = path.join(url, "file-notes.json");
+    let content =
+      fs.readFileSync(jsonPath, {
+        encoding: "utf-8",
+      }) || "{}";
+    notes = JSON.parse(content) || {};
   }
 }
 export function save2JSON() {
   let settingJsonPath = (vscode.workspace.workspaceFolders || [])[0]?.uri
     .fsPath;
   let url = path.join(settingJsonPath, ".vscode");
-  fs.writeFileSync(path.join(url, "file-notes.json"), JSON.stringify(notes));
+  fs.writeFileSync(
+    path.join(url, "file-notes.json"),
+    JSON.stringify(notes, null, "\t")
+  );
 }
